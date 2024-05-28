d44eb5c98e4752f50e8d5dc6366b617604c8b45b
Cameron Myers <45009513+cameron-myers@users.noreply.github.com>
2024-05-28 14:42:34 -0700

    init2

---
+++ main.cpp
@@ -0,0 +1,10 @@
+// Computer_Networks_Project.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <winsock.h>
+int main(int argc, char* argv[])
+{
+    std::cout << "Hello World!\n";
+}
+

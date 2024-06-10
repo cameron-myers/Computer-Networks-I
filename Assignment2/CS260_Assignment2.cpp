ce6081765e4b19ca354a91e7cbcd2375fe896537
Cameron Myers <45009513+cameron-myers@users.noreply.github.com>
2024-06-10 16:48:21 -0700

    assignment 2 complete

--- Assignment2/CS260_Assignment2.cpp
+++ Assignment2/CS260_Assignment2.cpp
@@ -1,20 +1,151 @@
-// CS260_Assignment2.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+
+#pragma comment( lib, "WS2_32.lib" )
 
 #include <iostream>
+#include <chrono>
+#include <thread>
+#include <winsock2.h>
+#include <WS2tcpip.h>
 
-int main()
+int main(int argc, char* argv[])
 {
-    std::cout << "Hello World!\n";
+
+    u_long iMode = 1;
+
+    //INIT WINSOCK/////////////////////////////////
+    WSADATA wsa_data;
+    int res = WSAStartup(MAKEWORD(2, 2), &wsa_data);
+    if (res != 0)
+    {
+        std::cerr << "Error in WSAStartup: " << WSAGetLastError() << std::endl;
+        return 1;
+    }
+    ///////////////////////////////////////////////
+
+
+    //CREATE SOCKET.///////////////////////////////
+    SOCKET tcp_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (tcp_socket == INVALID_SOCKET)
+    {
+        std::cerr << "failed to create the socket" << WSAGetLastError() << std::endl;
+        return 1;
+    }
+    ///////////////////////////////////////////////
+
+
+
+    //CREATE ADDRESS///////////////////////////////
+    //52.12.14.56
+    //8888
+    sockaddr_in myAddr = sockaddr_in();
+    memset(&myAddr.sin_zero, 0, 8);
+    myAddr.sin_family = AF_INET;
+    myAddr.sin_port = htons(8888); //port 8888
+
+    res = inet_pton(AF_INET, "52.12.14.56", &myAddr.sin_addr);
+    if (res != 1)
+    {
+        std::cerr << "inet_pton failed " << WSAGetLastError() << std::endl;
+        return 1;
+    }
+	///////////////////////////////////////////////
+
+
+	//CONNECT TO SERVER////////////////////////////
+    res = connect(tcp_socket,(const sockaddr*)&myAddr, (int)sizeof(myAddr));
+    if(res != 0)
+    {
+        std::cerr << "failed to connect to the server" << WSAGetLastError() << std::endl;
+        return 1;
+    }
+
+    ///////////////////////////////////////////////
+
+
+    //SET SOCKET TO NON-BLOCKING///////////////////
+    res = ioctlsocket(tcp_socket, FIONBIO, &iMode);
+    if (res != 0)
+    {
+        std::cerr << "failed to set to non-blocking mode" << WSAGetLastError() << std::endl;
+        return 1;
+    }
+    ///////////////////////////////////////////////
+
+
+    //SEND DATA////////////////////////////////////
+    int bytes_sent = sendto(tcp_socket, argv[0], (int)strlen(argv[0]), 0, (const sockaddr*)&myAddr, (int)sizeof(myAddr));
+    if (res != 0)
+    {
+        std::cerr << "failed to send data to the server" << WSAGetLastError() << std::endl;
+        return 1;
+    }
+    ///////////////////////////////////////////////
+
+    //DONE SENDING/////////////////////////////////
+    res = shutdown(tcp_socket, SD_SEND);
+    if (res != 0)
+    {
+        std::cerr << "failed to send shutdown signal" << WSAGetLastError() << std::endl;
+        return 1;
+    }
+
+    //RECEIVE//////////////////////////////////////
+    int bufferSize = 1500;
+    std::string output;
+    char* recv_buff = new char[bufferSize];
+    memset(recv_buff, 0, bufferSize);
+
+    while (true)
+    {
+        res = recvfrom(tcp_socket, recv_buff, bufferSize, 0, NULL, NULL);
+        output += recv_buff;
+        //clear out buffer for next recv
+        memset(recv_buff, 0, bufferSize);
+        //check for end
+        if (res == 0) break;
+
+    	//catch possible errors
+        if (res == SOCKET_ERROR)
+        {
+            //check for non fatal errors
+            if(WSAGetLastError() != 10035)
+            {
+                std::cerr << "recvfrom failed " << WSAGetLastError() << std::endl;
+                return 1;
+            }
+        }
+        //wait for next recv
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        std::cout << ". ";
+
+    }
+    
+
+    std::cout << output << std::endl;
+    delete[] recv_buff;
+
+    ///////////////////////////////////////////////
+
+
+    //CLOSE SOCKET/////////////////////////////////
+
+    res = closesocket(tcp_socket);
+    if (res != 0)
+    {
+        std::cerr << "closesocket failed " << WSAGetLastError() << std::endl;
+        return 1;
+    }
+    ///////////////////////////////////////////////
+
+    //CLEANUP /////////////////////////////////////
+
+    res = WSACleanup();
+    if (res != 0)
+    {
+        std::cerr << "WSACleanup failed" << WSAGetLastError() << std::endl;
+    }
+
+    return 0;
+    ///////////////////////////////////////////////
+
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
